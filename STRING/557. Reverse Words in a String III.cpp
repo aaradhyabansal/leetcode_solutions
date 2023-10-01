d14edc40877a33dd9981a0869695226b3057dc16
aaradhyabansal <aaradhyabansal21@gmail.com>
2023-10-01 09:32:40 +0530

    added 557. Reverse Words in a String III

---
+++ STRING/557. Reverse Words in a String III.cpp
@@ -0,0 +1,52 @@
+/*Given a string s, reverse the order of characters in each word within a sentence,
+ while still preserving whitespace and initial word order.*/
+ /*Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+Example 2:
+
+Input: s = "God Ding"
+Output: "doG gniD"
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+s contains printable ASCII characters.
+s does not contain any leading or trailing spaces.
+There is at least one word in s.
+All the words in s are separated by a single space.*/
+
+//SOL1 using sliding window
+
+class Solution {
+public:
+    void reverseString(string& s,int i,int j)
+    {
+        int n=j-i+1;
+        while(i<=j)
+        {
+            swap(s[i],s[j]);
+            i++;
+            j--;
+        }
+    }
+
+    string reverseWords(string s) {
+        int i=0,j=0;
+        while(j!=s.length())
+        {
+            if(s[j]==' ')
+            {
+                reverseString(s,i,j-1);
+                i=j+1;
+                j++;
+            }
+            j++;
+            
+        }
+        reverseString(s,i,j-1);
+        return s;
+    }
+};
