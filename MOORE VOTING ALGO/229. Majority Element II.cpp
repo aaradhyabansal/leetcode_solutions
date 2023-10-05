b5e12e52acfc8c94bb0947fc531172855643cb9b
aaradhyabansal <aaradhyabansal21@gmail.com>
2023-10-05 22:35:44 +0530

    229. Majority Element II

---
+++ MOORE VOTING ALGO/229. Majority Element II.cpp
@@ -0,0 +1,45 @@
+/*Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.*/
+
+/*Example 1:
+
+Input: nums = [3,2,3]
+Output: [3]
+Example 2:
+
+Input: nums = [1]
+Output: [1]
+Example 3:
+
+Input: nums = [1,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+-109 <= nums[i] <= 109
+*/
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]]++;
+        }
+        for(auto it:mp)
+        {
+            if(it.second>(n/3))
+            ans.push_back(it.first);
+        }
+        return ans;
+
+
+    }
+};
+T.C.-O(n)(worst Case)/O(nlogn)(avg case)
+S.C.-O(n)
+
+
