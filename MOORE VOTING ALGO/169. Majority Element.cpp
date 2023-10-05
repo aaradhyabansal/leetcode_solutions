ba91f96881170b35c4c0efe25aaaaa097ee06672
aaradhyabansal <aaradhyabansal21@gmail.com>
2023-10-05 22:46:57 +0530

    229. Majority Element I using moore's voting algo

--- MOORE VOTING ALGO/169. Majority Element.cpp
+++ MOORE VOTING ALGO/169. Majority Element.cpp
@@ -17,22 +17,53 @@ Constraints:
 n == nums.length
 1 <= n <= 5 * 104
 -109 <= nums[i] <= 109*/
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int,int>mp;
+//         int n=nums.size();
+//        int ans;
+//         for(int i=0;i<n;i++)
+//         {
+//             mp[nums[i]]++;
+//         }
+//         for(auto it:mp)
+//         {
+//             if(it.second>(n/2))
+//            { 
+//                return it.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        int n=nums.size();
-       int ans;
-        for(int i=0;i<n;i++)
+        //sol2 using moores voting algo
+        int el=INT_MIN,cnt=0;
+        for(int i=0;i<nums.size();i++)
         {
-            mp[nums[i]]++;
-        }
-        for(auto it:mp)
-        {
-            if(it.second>(n/2))
-           { 
-               return it.first;
+            if(cnt==0)
+            {
+                cnt=1;
+                el=nums[i];
             }
+            else if(nums[i]==el)
+            cnt++;
+            
+            else
+            cnt--;
+        }
+        cnt=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==el)
+            cnt++;
+
+            if(cnt>(nums.size()/2))
+            return el;
         }
         return -1;
     }
