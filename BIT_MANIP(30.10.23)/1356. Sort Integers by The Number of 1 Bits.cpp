6d6139a1894375cb1584339282223428e6c7b8da
aaradhyabansal <aaradhyabansal21@gmail.com>
2023-10-30 23:49:58 +0530

    1356. Sort Integers by The Number of 1 Bits

--- BIT_MANIP(30.10.23)/1356. Sort Integers by The Number of 1 Bits.cpp
+++ BIT_MANIP(30.10.23)/1356. Sort Integers by The Number of 1 Bits.cpp
@@ -99,3 +99,27 @@ int findOneBits(int num)
 
     }
 };
+//sol-3
+class Solution {
+public:
+
+
+    vector<int> sortByBits(vector<int>& arr) {
+        
+
+        auto comp=[&](int&a,int&b){
+            int cnt_a=__builtin_popcount(a);
+        int cnt_b=__builtin_popcount(b);
+
+        if(cnt_a==cnt_b)
+        return a<b;
+
+        return cnt_a<cnt_b;
+
+        };
+
+        sort(arr.begin(),arr.end(),comp);
+        return arr;
+
+    }
+};
